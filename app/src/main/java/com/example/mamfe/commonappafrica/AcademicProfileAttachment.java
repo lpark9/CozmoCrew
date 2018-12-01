4455d9f84fc25ed432a0840592cdca2293a90d2c
Lena Park <lpark9@gatech.edu>
2018-12-01 14:17:15 -0500

    Screenshot update

--- app/src/main/java/com/example/mamfe/commonappafrica/AcademicProfileAttachment.java
+++ app/src/main/java/com/example/mamfe/commonappafrica/AcademicProfileAttachment.java
@@ -2,6 +2,8 @@ package com.example.mamfe.commonappafrica;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -9,7 +11,9 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.RecoverySystem;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,186 +22,100 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
+import android.content.Context;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.UUID;
+
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AcademicProfileAttachment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AcademicProfileAttachment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import static android.app.Activity.RESULT_OK;
+
+
 public class AcademicProfileAttachment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button saveButton, uploadButton, prevButton, nextButton;
 
+    private ImageView idImage;
+    private Uri filePath;
+    private BroadcastReceiver mDownloadReceiver;
+    private FirebaseAuth mAuth;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    private FirebaseUser user;
+    private FirebaseDatabase database;
+    private final int PICK_IMAGE_REQUEST = 71;
+    private AcademicProfileApplicationDetails.OnFragmentInteractionListener mListener;
     private boolean isApplying;
-
-    private OnFragmentInteractionListener mListener;
-
-    public AcademicProfileAttachment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AcademicProfileAttachment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AcademicProfileAttachment newInstance(String param1, String param2) {
-        AcademicProfileAttachment fragment = new AcademicProfileAttachment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Uri mDownloadUrl = null;
+    private Uri mFileUri = null;
+    private static final String KEY_FILE_URI = "key_file_uri";
+    private static final String KEY_DOWNLOAD_URL = "key_download_url";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
-        Bundle bundle = this.getArguments();
-        if (bundle != null) {
-            isApplying = bundle.getBoolean("isApplying");
-        } else {
-            isApplying = false;
-        }
-
-
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == Activity.RESULT_OK)
-            switch (requestCode){
-                case 6:
-                    Uri selectedImage = data.getData();
-
-                    updateFileHandle(selectedImage.getPath());
-                    try {
-                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImage);
-                        ((ImageView) getView().findViewById(R.id.profileImage)).setImageBitmap(bitmap);
-
-                    } catch (IOException e) {
-                        Log.i("TAG", "Some exception " + e);
-                    }
-                    break;
-            }
-    }
-
-    private void tryPopulateImage() {
-
-        try {
-
-            File directory = getActivity().getFilesDir();
-            File file = new File(directory, "picture_path");
-
-            FileInputStream fs = new FileInputStream(file);
-            byte[] data = new byte[(int) file.length()];
-            fs.read(data);
-            fs.close();
-
-            String imagePath = new String(data, "UTF-8");
-
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            Bitmap bitmap = BitmapFactory.decodeFile(imagePath, bmOptions);
-            ((ImageView) getView().findViewById(R.id.profileImage)).setImageBitmap(bitmap);
-
-        } catch(Exception e) {
-
-            System.out.println("MMMMMMS");
-            e.printStackTrace();
-        }
-
-
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        tryPopulateImage();
-    }
-
-    private void updateFileHandle(String handle) {
-
-        File directory = getActivity().getFilesDir();
-        File file = new File(directory, "picture_path");
-
-        try {
-            file.createNewFile();
-            FileOutputStream fOut = new FileOutputStream(file);
-            OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
-            myOutWriter.append(handle);
-
-            myOutWriter.close();
-
-            fOut.flush();
-            fOut.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        //storageReference = FirebaseStorage.getInstance().getReference();
+//        if (savedInstanceState != null) {
+//            mFileUri = savedInstanceState.getParcelable(KEY_FILE_URI);
+//            mDownloadUrl = savedInstanceState.getParcelable(KEY_DOWNLOAD_URL);
+//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_academic_profile_attachment, container, false);
 
-        //Bind the save listener to button
-        Button saveButton = view.findViewById(R.id.saveButton);
-        Button nextButton = view.findViewById(R.id.next_button);
-        Button prevButton = view.findViewById(R.id.prev_button);
-        Button attachButton = view.findViewById(R.id.chooseImageButton);
 
-        attachButton.setOnClickListener(new View.OnClickListener() {
+//        mAuth = FirebaseAuth.getInstance();
+//        user = mAuth.getCurrentUser();
+
+//
+//        //final String uid = user.getUid();
+//
+//        //Bind the save listener to button
+        uploadButton = view.findViewById(R.id.chooseImageButton);
+        saveButton = view.findViewById(R.id.saveButton);
+        nextButton = view.findViewById(R.id.next_button);
+        idImage = view.findViewById(R.id.profileImage);
+        prevButton = view.findViewById(R.id.prev_button);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, 6);
+            public void onClick(View v) {
+                chooseImage();
             }
         });
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast feedback = Toast.makeText(view.getContext(), "Information Updated!", Toast.LENGTH_SHORT);
-                feedback.show();
+                //uploadImage();
+//                Toast feedback = Toast.makeText(view.getContext(), "Information Updated!", Toast.LENGTH_SHORT);
+//                feedback.show();
+
             }
         });
+
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -228,47 +146,61 @@ public class AcademicProfileAttachment extends Fragment {
             }
         });
 
-
-        // Inflate the layout for this fragment
         return view;
     }
+//
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), filePath);
+                idImage.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
+//
+//    private void uploadImage() {
+//
+//        if (filePath != null) {
+//
+//            if (storageReference !=null) {
+//                StorageReference ref = storageReference.child("images/" + UUID.randomUUID().toString());
+//                ref.putFile(filePath)
+//                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                            @Override
+//                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                                Toast text = Toast.makeText(getContext(), "Uploaded!", Toast.LENGTH_SHORT);
+//                                text.show();
+//
+//
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//
+//                                Toast text = Toast.makeText(getContext(), "Failed.", Toast.LENGTH_SHORT);
+//                                text.show();
+//                            }
+//                        });
+//            } else{
+//                Toast text = Toast.makeText(getContext(), "Database Sink Failed.", Toast.LENGTH_SHORT);
+//                text.show();
+//            }
 //        }
-    }
+//    }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
